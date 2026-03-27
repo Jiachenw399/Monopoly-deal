3082252699aaeb6b7797995bf79557a86dcacbf8
24372224 <2716653751@qq.com>
2026-03-27 22:50:18 +0800

    。

--- Monopoly Deal/src/GUI/Main.java
+++ Monopoly Deal/src/GUI/Main.java
@@ -4,9 +4,6 @@ import 基础类.DrawCards;
 
 public class Main {
     static void main() {
-        DrawCards dc = new DrawCards();
-        System.out.println(dc.getDrawCards().getFirst().getValue());
-        System.out.println(dc.getDrawCards().getLast().getValue());
-        System.out.println(dc.getDrawCards().size());
+
     }
 }
