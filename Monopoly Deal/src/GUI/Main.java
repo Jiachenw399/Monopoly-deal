e8bd5a68eb52406eeb8503f5c01d8446ccecbc45
24372224 <2716653751@qq.com>
2026-03-29 17:44:32 +0800

    改好

--- Monopoly Deal/src/GUI/Main.java
+++ Monopoly Deal/src/GUI/Main.java
@@ -1,10 +1,23 @@
 package GUI;
 
+import 基础类.ActionCards;
+import 基础类.Player;
+import 基础类.PropertiesCards;
 import 逻辑类.Game;
 
 public class Main {
     static void main() {
         Game g = new Game();
-        System.out.println(g.getDrawCards().getDrawCards().getFirst().getValue());
+        int a = 0;
+        System.out.println(g);
+        for (int i = 0; i < g.getDrawCards().getDrawCards().size(); i++) {
+            if(g.getDrawCards().getDrawCards().get(i).getClass().equals(ActionCards.class)) {
+                a+=1;
+            }
+            if(g.getDrawCards().getDrawCards().get(i).getClass().equals(PropertiesCards.class)) {
+                a+=1;
+            }
+        }
+        System.out.println(a);
     }
 }
