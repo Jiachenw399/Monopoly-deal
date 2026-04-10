867615ed6317150d9a82d52fcc2c37a1bc2baec5
lijunxuan <m13021106218@163.com>
2026-04-10 08:49:07 +0800

    运行报错问题

--- Monopoly fx/src/main/java/module-info.java
+++ Monopoly fx/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module Monopoly.fx {
     requires javafx.fxml;
     requires javafx.swt;
     requires java.datatransfer;
+    requires java.desktop;
 
     opens GUI to javafx.fxml; // 启动时使用
     exports GUI; // 导出 GUI 模块
